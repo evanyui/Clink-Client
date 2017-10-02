db61e7aa320e45c24072428288861b952155a0b2
evanyui <evanyui@hotmail.com>
2017-10-01 21:46:12 -0400

    Enable add multiple tags

--- js/index.js
+++ js/index.js
@@ -8,25 +8,27 @@ $(function() {
 
             // Send tab's address with the tag as a pair to the server
             if(isValidURL(url)) {
-                socket.emit('post', $('#tag').val(), url);
+                socket.emit('post', $('#tags').val(), url);
             } else {
-                $('#tag').attr('placeholder', 'Current tab is not an url'); 
+                $('#tags').attr('placeholder', 'Current tab is not an url'); 
             }
 
             // Empty the textfield
-            $('#tag').val('');
+            $('#tags').val('');
         });
         return false;
     });
 
+    // TODO: Will be replaced by only when user query server/db
+    // FOR TESTING
     // When client receive data from server
-    socket.on('receive', function(tag, link) {
+    socket.on('receive', function(tags, link) {
         $('#linklist').append($('<li>').text(link));
     });
 });
 
 // Predicate to check if tab address is a url with regex
-function isValidURL(str) {
+var isValidURL = function(str) {
     var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
     if(!regex .test(str)) {
         return false;
