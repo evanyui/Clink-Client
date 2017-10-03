31a4a196cdd6b9c1c70542d78c457adc9027a356
evanyui <evanyui@hotmail.com>
2017-10-03 05:15:20 -0400

    Implement search box to query to server

--- js/index.js
+++ js/index.js
@@ -1,32 +1,56 @@
 $(function() {
+    // When user clink is opened, user is connected to the server
     var socket = io.connect('http://localhost:3000');
-    $('form').submit(function() {
-        // Get current tab
-        chrome.tabs.query({'active': true, 'currentWindow': true}, function (tabs) {
-            // Get tab's address
-            var url = tabs[0].url;
 
-            // Send tab's address with the tag as a pair to the server
-            if(isValidURL(url)) {
-                socket.emit('post', $('#tags').val(), url);
-            } else {
-                $('#tags').attr('placeholder', 'Current tab is not an url'); 
-            }
+    // Get current tab
+    var url;
+    chrome.tabs.query({'active': true, 'currentWindow': true}, function (tabs) {
+        // Get tab's address
+        url = tabs[0].url;
+    });
 
-            // Empty the textfield
-            $('#tags').val('');
-        });
+    // When user submit form to post link
+    $('#post-form').on('submit', function(event) {
+        // TODO: weird bug on html having two submit forms
+        // Check if works on other browser
+        event.preventDefault();
+
+        // Send tab's address with the tag as a pair to the server
+        if(isValidURL(url)) {
+            socket.emit('post', $('#tags').val(), url);
+        } else {
+            //TODO: need better way to present info message
+            $('#tags').attr('placeholder', 'Current tab is not an url'); 
+        }
+
+        // Empty the textfield
+        $('#tags').val('');
+
         return false;
     });
 
-    // TODO: Will be replaced by only when user query server/db
-    // FOR TESTING
+    // When user submit form to query links
+    $('#query-form').on('submit', function(event) {
+        // TODO: weird bug on html having two submit forms
+        // Check if works on other browser
+        event.preventDefault();
+
+        // Query to server
+        socket.emit('query', $('#query').val(), url);
+
+        return false;
+    });
+
     // When client receive data from server
-    socket.on('receive', function(tag, link) {
-        $('#linklist').append(
-            $('<li/>').append(
-                $('<a/>').attr({href: link, target: '_blank'})
-                         .text(tag)));
+    // only after client query links to server
+    // query results are sent back in array
+    socket.on('receive', function(results) {
+        results.forEach(function(res) {
+            $('#linklist').append(
+                $('<li/>').append(
+                    $('<a/>').attr({href: res.url, target: '_blank'})
+                             .text(res.key)));
+        });
     });
 });
 
