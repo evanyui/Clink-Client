ed5630d92cc1820e61d157efe1e88fc4dba4eac5
evanyui <evanyui@hotmail.com>
2018-03-06 11:00:06 -0500

    add jquery and material icons

--- js/inject.js
+++ js/inject.js
@@ -3,12 +3,12 @@
     var jquery = document.createElement('script');
     jquery.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
     jquery.type = 'text/javascript';
-    document.head.appendChild(jquery);
+    document.appendChild(jquery);
 
     var materialIcons = document.createElement('link');
     materialIcons.rel = "stylesheet";
     materialIcons.href= "https://fonts.googleapis.com/icon?family=Material+Icons";
-    document.head.appendChild(materialIcons);
+    document.appendChild(materialIcons);
 
     const openWidth = "350px";
 
