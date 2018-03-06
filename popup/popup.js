db563bc699d3e59f9e320d1f81d7a4da0072c561
evanyui <evanyui@hotmail.com>
2018-03-06 09:52:26 -0500

    testing html injection

--- popup/popup.js
+++ popup/popup.js
@@ -1,13 +1,7 @@
-// Get address using chrome api
-// chrome.tabs.query({'active': true, 'currentWindow': true}, function (tabs) {
-//     // Get tab's address
-//     var url = tabs[0].url;
-// });
-
 // Send script to current page
 chrome.tabs.executeScript(
     null, 
-    {file: "/popup/inject.js"}
+    {file: "/popup/inject.js", "/html/inject.html"}
 );
 
 // If debugging with logging, comment out this line
