1306cb5ee7c4ca7452ed7e90d909ae29a8635fa9
evanyui <evanyui@hotmail.com>
2018-03-06 09:54:53 -0500

    testing html injection

--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,13 @@
+
+chrome.extension.getURL('html/inject.html'), function(data) {
+    document.body.appendChild (data);
+}
+
 // Send script to current page
 chrome.tabs.executeScript(
     null, 
-    {file: "/popup/inject.js", "/html/inject.html"}
+    {file: "/popup/inject.js"}
 );
 
 // If debugging with logging, comment out this line
-// window.close();
+window.close();
