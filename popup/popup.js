6ea636c01930e38533c282e9957bd2870ee325e6
evanyui <evanyui@hotmail.com>
2018-03-13 10:39:29 -0400

    fix scrollbar visibility

--- popup/popup.js
+++ popup/popup.js
@@ -4,5 +4,10 @@ chrome.tabs.executeScript(
     {file: "/js/inject.js"}
 );
 
+// chrome.browserAction.onClicked.addListener(function(activeTab){
+//   var newURL = "http://www.clink.live/";
+//   chrome.tabs.create({ url: newURL });
+// });
+
 // If debugging with logging, comment out this line
 window.close();
