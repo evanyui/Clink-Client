f3bd8803a4ea06c23e91de14682be90daa87c8b5
evanyui <evanyui@hotmail.com>
2018-03-13 09:12:03 -0400

    create style js for page

---
+++ js/pageStyle.js
@@ -0,0 +1,32 @@
+$(function() {
+    $('#query-form').on('submit', function(event) {
+        rotate(720);
+        hide();
+    });
+});
+
+var rotate = function(d) {
+    $({deg: 0}).animate({deg: d}, {
+        step: function(now, fx){
+            $("#logo").css({
+                 transform: "rotate(" + now + "deg)"
+            });
+        }
+    });
+}
+
+// var hide = function() {
+//     $('#upper').css('height', '5%');
+//     $('#middle').css('top', 'calc(5% - 17px)');
+//     $('#form-div-shadow').css('top', 'calc(5% - calc(34px / 2))');
+//     $('#lower').css('height', '90%');
+//     $('#logo').hide();
+// }
+
+// var show = function() {
+//     $('#upper').css('height', '15%');
+//     $('#middle').css('top', 'top: calc(15% - 17px)');
+//     $('#form-div-shadow').css('top', 'calc(15% - calc(34px / 2))');
+//     $('#lower').css('height', '80%');
+//     $('#logo').show();
+// }
